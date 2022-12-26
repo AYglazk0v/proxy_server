e4a4c60f64f2b8b05f2f01a2a724711a7ceaaee6
ayglazkov <AYGlazkov@stud.kpfu.ru>
2022-12-27 00:00:09 +0300

    Done and echo_server

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv){
     ManagerServer* server= new ManagerServer();
-    server->Add("127.0.0.1", 2121, "127.0.0.1", 5555);
+    server->Add("127.0.0.1", 12345, "127.0.0.1", 5555);
     server->Handle();
     delete server;
     return 0;
